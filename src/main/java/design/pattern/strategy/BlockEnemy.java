7edaccbb9d38d9c84b2c28e3565b8eb9c5995ae9
priester <853018334@qq.com>
2017-05-11 16:28:39 +0800

    note

---
+++ src/main/java/design/pattern/strategy/BlockEnemy.java
@@ -0,0 +1,11 @@
+package design.pattern.strategy;
+
+/*
+ * 孙夫人断后，挡住追兵
+ */
+public class BlockEnemy implements IStrategy {
+	public void operate() {
+
+		System.out.println("孙夫人断后，挡住追兵");
+	}
+}
---
+++ src/main/java/design/pattern/strategy/GivenGreenLight.java
@@ -0,0 +1,10 @@
+package design.pattern.strategy;
+
+/*
+ * 求吴国太开个绿灯
+ */
+public class GivenGreenLight implements IStrategy {
+	public void operate() {
+		System.out.println("求吴国太开个绿灯,放行！");
+	}
+}
